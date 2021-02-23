92c15d1215f6e8bdfbb410504a96fd9daa5ba371
Fignis <j.huang098@gmail.com>
2021-02-22 19:48:17 -0500

    encountered bug will fix tomorrow

--- webscrap/index.js
+++ webscrap/index.js
@@ -1,9 +1,14 @@
-import { getHtml, getEbayPrices, formatResults } from "./lib/scrape";
+import {
+  getHtml,
+  getClHtml,
+  getEbayPrices,
+  formatResults,
+} from "./components/scrape";
 
-async function go() {
+const go = async () => {
   const htmlData = await getHtml("Basketball");
-
   const formattedEbayRes = await formatResults(htmlData);
-  console.log(`Here is a list of prices:${formattedEbayRes}.`);
-}
+
+  console.log(formattedEbayRes);
+};
 go();
