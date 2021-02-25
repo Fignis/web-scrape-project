9188d7af7f9a18ded18e020cb6a8c8c617e77cb7
Fignis <j.huang098@gmail.com>
2021-02-24 22:56:12 -0500

    changed shipping info for ebay scrap to display correct info, still working on pagination

--- webscrap/components/scrape.js
+++ webscrap/components/scrape.js
@@ -14,6 +14,7 @@ const getHtml = async (
         "User-Agent": "Node.js",
       },
     });
+    console.log(url);
     return data;
   } catch (err) {
     console.log(err.response);
@@ -39,7 +40,9 @@ const getEbayPrices = ($) => {
     .text()
     .trim();
   const shippingInfo = pathForPriceInfo
-    .find("div[class='s-item__detail s-item__detail--primary']")
+    .find(
+      "div[class='s-item__detail s-item__detail--primary']> span[class='s-item__shipping s-item__logisticsCost']"
+    )
     .text()
     .trim();
 
@@ -64,7 +67,8 @@ const formatResults = async (data) => {
     .get();
   const numberOfResults = mapResults.length - 1;
   numberOfMappedResults = numberOfResults;
-  return `${numberOfResults}, ${JSON.stringify(mapResults, null, 2)}`;
+
+  return `${numberOfResults},${JSON.stringify(mapResults, null, 2)}`;
 };
 const getClHtml = async (searchTerm) => {
   try {
@@ -77,26 +81,37 @@ const getClHtml = async (searchTerm) => {
   }
 };
 const scrapperEbay = async (searchTerm) => {
-  getHtml(searchTerm);
-  const displayedResults = formatResults(getHtml(searchTerm));
-  if (numberOfMappedResults < 1) {
-    //in order to view the actual data we need to convert the array to a string and return it.
-    return displayedResults;
-  }
-  {
-    let url = `https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=${searchTerm}&_sacat=0&LH_TitleDesc=0&_pgn=1`;
-    //so we need to get the number of the next page which is done by this regular expression.
-    //im not quite familiar with regular expressions however, what this does it gets the last portion of the url which contains the page number
-    //it turns the string into a number value with the parseInt function
-    //then it adds one to the value that was extracted.
-    const numberOfNextPage = parseInt(url.match(/pgn=(\d+)$/)[1], 10) + 1;
-    //after the numberOfNextPage is retrieved, concatenate it into the url to retrieve the next page of the search results.
-    const nextPage = `https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=${searchTerm}&_sacat=0&LH_TitleDesc=0&_pgn=${numberOfNextPage}`;
-    const nextPageDat = getHtml(searchTerm, nextPage);
-    console.log(nextPage);
-    return formatResults(nextPageDat);
+  try {
+    const htmlData = await getHtml(searchTerm);
+    const displayedResults = await formatResults(htmlData);
+    console.log(numberOfMappedResults);
+
+    //Recursive approach to pagination doesnt work very buggy requires me to make the getHtml() to recieve a URL as a parameter instead of a searchTerm
+
+    if (numberOfMappedResults === 0) {
+      //in order to view the actual data we need to convert the array to a string and return it.
+      return displayedResults;
+    } else {
+      let url = `https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=${searchTerm}&_sacat=0&LH_TitleDesc=0&_pgn=1`;
+      //so we need to get the number of the next page which is done by this regular expression.
+      // regex gets the last portion of the url which contains the page number
+      //it turns the string into a number value with the parseInt function
+      //then it adds one to the value that was extracted.
+      const numberOfNextPage = parseInt(url.match(/pgn=(\d+)$/)[1], 10) + 1;
+      //after the numberOfNextPage is retrieved, concatenate it into the url to retrieve the next page of the search results.
+      const nextPage = `https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=${searchTerm}&_sacat=0&LH_TitleDesc=0&_pgn=${numberOfNextPage}`;
+      const nextPageDat = await getHtml(searchTerm, nextPage);
+
+      const nextPageRes = await formatResults(nextPageDat);
+      console.log(displayedResults.concat(nextPageRes));
+      return displayedResults.concat(nextPageRes);
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
+/*
+const getKehData = () => {};
 /*const getClprices = ($) => {
   const clSearchRes = $.find(
     'div[class="bq4bzpyk j83agx80 btwxx1t3 lhclo0ds jifvfom9 muag1w35 dlv3wnog enqfppq2 rl04r1d5"]'
