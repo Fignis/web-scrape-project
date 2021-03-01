a223350467155f94845c9c429942d77185d5a220
Fignis <j.huang098@gmail.com>
2021-03-01 17:08:32 -0500

    Pagination works! connecting to a db will be next.

--- webscrap/index.js
+++ webscrap/index.js
@@ -1,7 +1,10 @@
-import { scrapperEbay } from "./components/scrape";
+import { scrapperEbay /*, formatCl, getClHtml*/ } from "./components/scrape";
 
 const go = async () => {
-  const formattedEbayRes = await scrapperEbay("Helios44-2");
-  console.log(`here are the results: ${formattedEbayRes}`);
+  const formattedEbayRes = await scrapperEbay("Takumar");
+  //const clHtml = await getClHtml("Takumar");
+  const formattedClRes = formatCl(clHtml);
+  console.log(JSON.stringify(formattedEbayRes, null, 2));
+  // console.log("Cl Stuff", formattedClRes);
 };
 go();
