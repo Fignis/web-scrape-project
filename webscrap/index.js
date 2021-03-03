868467947dc39afc17ef622db9109bce8394e77a
Fignis <j.huang098@gmail.com>
2021-03-02 19:08:24 -0500

    added new dependencies like express and mongodb, server doesnt seem to work will fix tomorrow

--- webscrap/index.js
+++ webscrap/index.js
@@ -1,10 +1,16 @@
 import { scrapperEbay /*, formatCl, getClHtml*/ } from "./components/scrape";
-
-const go = async () => {
+import express from "express";
+const app = express();
+const port = process.env.PORT_NUM || 7640;
+app.get("/scrape", async (req, res, next) => {
   const formattedEbayRes = await scrapperEbay("Takumar");
   //const clHtml = await getClHtml("Takumar");
   const formattedClRes = formatCl(clHtml);
   console.log(JSON.stringify(formattedEbayRes, null, 2));
+  res.json(formattedEbayRes);
   // console.log("Cl Stuff", formattedClRes);
-};
-go();
+});
+
+app.listen(port, () => {
+  console.log(`server is running!!: ${port}`);
+});
---
+++ webscrap/models/listing.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const schema = mongoose.Schema;
+
+const listingSchema = new schema({
+    _id:schema.Types.ObjectId,
+    title: {
+        type:String,
+        requried:true,
+        unique:true
+    },
+    subtitle:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    shipping:{
+        type:String,
+        required:true
+    }
+
+})
