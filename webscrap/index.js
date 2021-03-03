0c83150c67743f3d312775844541a0f2e72b837e
Fignis <j.huang098@gmail.com>
2021-03-03 16:29:20 -0500

    connection to db not finished and also data takes forever to load to express.

--- webscrap/index.js
+++ webscrap/index.js
@@ -1,16 +1,25 @@
 import { scrapperEbay /*, formatCl, getClHtml*/ } from "./components/scrape";
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import listing from "./models/listing";
+
 const app = express();
 const port = process.env.PORT_NUM || 7640;
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 app.get("/scrape", async (req, res, next) => {
   const formattedEbayRes = await scrapperEbay("Takumar");
   //const clHtml = await getClHtml("Takumar");
-  const formattedClRes = formatCl(clHtml);
-  console.log(JSON.stringify(formattedEbayRes, null, 2));
-  res.json(formattedEbayRes);
+  //console.log(JSON.stringify(formattedEbayRes, null, 2));
+  res.json(formattedEbayRes, 2);
+  res.end();
   // console.log("Cl Stuff", formattedClRes);
 });
 
 app.listen(port, () => {
   console.log(`server is running!!: ${port}`);
 });
+
+var listing = new listing(formattedEbayRes);
--- webscrap/models/listing.js
+++ webscrap/models/listing.js
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const schema = mongoose.Schema;
 
 const listingSchema = new schema({
-    _id:schema.Types.ObjectId,
-    title: {
-        type:String,
-        requried:true,
-        unique:true
-    },
-    subtitle:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    shipping:{
-        type:String,
-        required:true
-    }
-
-})
+  _id: schema.Types.ObjectId,
+  title: {
+    type: String,
+    requried: true,
+    unique: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  shipping: {
+    type: String,
+    required: true,
+  },
+});
+export { listing };
